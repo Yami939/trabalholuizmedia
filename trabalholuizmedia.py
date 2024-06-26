57d67cabedb1805c9ec89bf9687e0ffc359dc664
Yami939 <150692981+Yami939@users.noreply.github.com>
2024-06-26 14:32:19 -0300

    prontin

---
+++ trabalholuizmedia.py
@@ -0,0 +1,13 @@
+print ('CALCULADORA DE MEDIA')
+
+print('Digite as notas do aluno: ')
+
+primeira_nota = float(input('primeira nota: '))
+segunda_nota = float(input('segunda nota: '))
+terceira_nota = float(input('terceira nota: '))
+quarta_nota = float(input('quarta nota: '))
+
+media = (primeira_nota + segunda_nota + terceira_nota + quarta_nota)
+
+print ('a nota final do aluno é:')
+print (media /4)
