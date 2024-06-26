9ef1bcafe4025e43070626948950dd9827bafbc1
Yami939 <150692981+Yami939@users.noreply.github.com>
2024-06-26 19:40:00 -0300

    ops

--- trabalholuizmedia.py
+++ trabalholuizmedia.py
@@ -1,5 +1,9 @@
 print ('CALCULADORA DE MEDIA')
 
+nome_do_aluno = input('Digite o nome do aluno: ')
+
+print(nome_do_aluno)
+
 print('Digite as notas do aluno: ')
 
 primeira_nota = float(input('primeira nota: '))
@@ -7,14 +11,14 @@ segunda_nota = float(input('segunda nota: '))
 terceira_nota = float(input('terceira nota: '))
 quarta_nota = float(input('quarta nota: '))
 
+
 notas = (primeira_nota + segunda_nota + terceira_nota + quarta_nota)
 media = (notas /4)
 
 if media >= 6:
-    print ('Aluno aprovado, nota final:')
+    print (nome_do_aluno,'fechou com: ')
 else:
-    print ('Aluno reprovado, nota final:')
-
+    print (nome_do_aluno,'fechou com: ')
 
 print (media)
 
